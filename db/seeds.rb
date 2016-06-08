7f4b04edc953b369c6f18aa288f4803f7739fe48
Lisa Autz <lisa.margarita.autz@gmail.com>
2016-06-08 18:38:42 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "The Legacy of Desertification In Violent Conflict", body: "Perhaps what is most peculiar about the so-called Arab Spring is that it arguably began in Tunisia with a simple fruit vendor desperate to make some money during a record high food price spike.
+
+Tunisian police harassed the fruit vendor, Mohammed Bouazizi, for not having a permit for selling food as cops are commonly reported doing--taking pleasure in fining, confiscating, and stealing from vendors at the fruit market. This time the merchant would stand his ground, and in return, he received the back end of a police baton for it.
+
+When he went to complain to government officials, they ignored his pleas, encouraging him to forget the entire incident. In the face of systematic injustice, Bouazizi decided to take drastic action. He resolved to attract more attention to his plight by setting his entire body on fire. His fire sparked millions in Tunisia to rise up against authority for the sake of vendors, Bouazizi, as well as their own dignity.", image: "desert.jpg")
+
+Article.create(title: "Uncontested Democracy: New York ballots offer voters startlingly little choice", body: "The strength of a democratic government is in its citizens' ability to choose their representatives. But if the choices are limited, is the process truly democratic?
+
+On Election Day - Tuesday, November 4 - more than a third of all races for seats in the New York State Legislature, 74 out of 213, feature a candidate running unopposed. The same holds true when you zoom in on New York City, where 21 of 63 races are uncontested.
+
+Looking at this startling democratic mutation, Gotham Gazette spoke with experts seeking answers as to why such a phenomenon exists and what can be done to make New York elections more competitive. The theory goes, of course, that more competition for office moves candidates to further engage with voters, explain specific policy positions, and remain more accountable to constituents if victorious.
+
+Along with its partner for this project, City Limits, Gotham Gazette also spoke with New Yorkers in several sections of the city where voters will not have candidate choice on Election Day.", image:"democracy.jpg")
+
+
+Article.create(title: " The New City Council: What’s Changed and What Hasn’t in 2014", body: "Horse carriages, chokeholds, and plastic bags. Legislation on these topics has recently been introduced to the New York City Council and shown marked division among its members.
+
+We look forward to a spirited debate, Council Member Rafael Espinal said in response to the introduction of a bill to outlaw horse-drawn carriages in New York City at the most recent full-body city council meeting, on December 8.
+
+Espinal's comment came after he expressed his disapproval of the ban and on the heels of several other council members, including the bill's lead sponsor Danny Dromm, speaking either for or against the legislation. Dromm introduced the much-hyped bill at the request of Mayor Bill de Blasio.
+
+The horse carriage bill will not be voted on until 2015, if at all, but it could provide for one of the rare instances when a bill comes through a council issue committee (it will be evaluated in transportation) and to the full council without near-unanimous support of the Council's 51 members, 48 of whom are Democrats.
+
+The City Council has also recently seen the introduction of several bills related to NYPD practices and one outlining a fee on plastic grocery bags that could all also lead to closer votes than any the legislative body has taken in all of 2014.
+
+Dozens of bills have been passed this year by the New York City Council, a total of 72 through December 8, and the most opposition any single bill has seen is six 'nay' votes. Even this level of opposition, six against, has only occurred on three of those 72 bills.
+
+(Note: six of the 72 were overrides of vetoes by Mayor Michael Bloomberg; four of the 72 have not yet been signed by Mayor Bill de Blasio)", image: "citycouncil.jpg")
